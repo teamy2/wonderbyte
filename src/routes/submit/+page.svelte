6aeff0d2992a17756d1877d8412c3dc6a07b7a02
Vasil <vasiltopalovic@outlook.com>
2023-11-17 20:01:07 -0500

    feat: convert image to base64

--- src/routes/submit/+page.svelte
+++ src/routes/submit/+page.svelte
@@ -2,16 +2,18 @@
 	import { onMount } from 'svelte';
 	let canvas: HTMLCanvasElement;
 	let video: HTMLVideoElement;
-	let photo: HTMLImageElement;
 
+	let file: HTMLInputElement;
+	let image: HTMLImageElement;
 	let width = 400;
 	let height = 400;
 
 	onMount(() => {
 		video = document.getElementById('video')! as HTMLVideoElement;
-
+		file = document.getElementById('file')! as HTMLInputElement;
 		canvas = document.getElementById('canvas')! as HTMLCanvasElement;
-		photo = document.getElementById('photo')! as HTMLImageElement;
+
+		image = document.getElementById('image')! as HTMLImageElement;
 		navigator.mediaDevices
 			.getUserMedia({ video: true, audio: false })
 			.then((stream) => {
@@ -31,19 +33,47 @@
 		ctx.drawImage(video, 0, 0, width, height);
 
 		const data = canvas.toDataURL('image/png');
-		photo.setAttribute('src', data);
+		image.setAttribute('src', data);
+	}
+
+	function submitFile() {
+		console.log(file.value);
+		let reader = new FileReader();
+		reader.onload = function (e) {
+			image.setAttribute('src', e.target!.result as string);
+		};
+		reader.readAsDataURL(file.files![0]);
+	}
+
+	function submitPicture() {
+		const ctx = canvas.getContext('2d')!;
+		ctx.drawImage(image, 0, 0, width, height);
+		const base64 = canvas.toDataURL('image/png');
+		console.log(base64);
 	}
 </script>
 
 <div class="flex flex-col justify-center items-center h-screen gap-4">
-	<h2 class=" text-xl">Upload your image!</h2>
-	<input type="file" class="file-input w-full max-w-xs input-bordered" />
+	<h2 class="text-xl">Upload your image!</h2>
+	<input
+		accept="image/*"
+		id="file"
+		type="file"
+		on:change={submitFile}
+		class="file-input w-full max-w-xs input-bordered"
+	/>
 
 	<h2>Or...</h2>
 
 	<video muted id="video">Video stream not available.</video>
-	<button id="start" on:click={takePicture}>Take photo</button>
-
+	<div class="flex gap-2">
+		<button id="start" class="btn input-bordered" on:click={takePicture}
+			>Take Photo</button
+		>
+		<button class="btn input-bordered" on:click={submitPicture}
+			>Submit Photo</button
+		>
+	</div>
 	<canvas id="canvas" {width} {height} class=" hidden" />
-	<img alt="Test" {width} {height} class=" border-white border-2" id="photo" />
+	<img alt="Test" {width} {height} id="image" />
 </div>
